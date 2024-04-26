01216de0211919176fe9fa62857b9d169e949e66
dnyaneshwarmagar <dnyaneshwarmagar276@gmail.com>
2024-04-26 17:52:03 +0530

    done1

--- src/components/Tooltip.js
+++ src/components/Tooltip.js
@@ -2,44 +2,42 @@
 import React, { useState } from 'react';
 
 const Tooltip = ({ text, children }) => {
-  const [showTooltip, setShowTooltip] = useState(false);
+    const [showTooltip, setShowTooltip] = useState(false);
 
-  const handleMouseEnter = () => {
-    setShowTooltip(true);
-  };
+    const handleMouseEnter = () => {
+        setShowTooltip(true);
+    };
 
-  const handleMouseLeave = () => {
-    setShowTooltip(false);
-  };
+    const handleMouseLeave = () => {
+        setShowTooltip(false);
+    };
 
-  return (
-    <div
-      onMouseEnter={handleMouseEnter}
-      onMouseLeave={handleMouseLeave}
-      style={{ position: 'relative', display: 'inline-block',margin:"30px"}}
-    >
-      {children}
-      {showTooltip && (
-        <span
-          style={{
-            // visibility: 'visible',
-            backgroundColor: 'red',
-            color: 'white',
-            borderRadius: '6px',
-            padding: '5px',
-            position: 'absolute',
-            zIndex: '1',
-            bottom: '125%',
-            marginLeft: '-60px',
-            opacity: '1',
-            transition: 'opacity 0.3s',
-          }}
+    return (
+        <div className="tooltip"
+            onMouseEnter={handleMouseEnter}
+            onMouseLeave={handleMouseLeave}
+            style={{ position: 'relative', display: 'inline-block', margin: "30px" }}
         >
-          {text}
-        </span>
-      )}
-    </div>
-  );
+            {children}
+            {showTooltip && (
+                <span className="tooltiptext"
+                    style={{
+                        // visibility: 'visible',
+                        backgroundColor: 'red',
+                        color: 'white',
+                        borderRadius: '6px',
+                        padding: '5px',
+                        position: 'absolute',
+                        zIndex: '1',
+                        bottom: '125%',
+                        marginLeft: '-60px',
+                    }}
+                >
+                    {text}
+                </span>
+            )}
+        </div>
+    );
 };
 
 export default Tooltip;
